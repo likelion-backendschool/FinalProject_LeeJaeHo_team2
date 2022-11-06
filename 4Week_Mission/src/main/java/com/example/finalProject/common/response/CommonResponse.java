dabf45dee7dba81123a231072e26f90b686c4a30
jhdl0157 <jhdl0157@naver.com>
2022-11-06 18:41:08 +0900

    Feat : 응답 클래스 정의

---
+++ 4Week_Mission/src/main/java/com/example/finalProject/common/response/CommonResponse.java
@@ -0,0 +1,37 @@
+package com.example.finalProject.common.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommonResponse<T> {
+    private String resultCode;
+    private String msg;
+    private T data;
+    private boolean fail;
+    public boolean success;
+
+    public static <T> CommonResponse<T> success(T data, String message) {
+        return (CommonResponse<T>) CommonResponse.builder()
+                .resultCode("S-1")
+                .msg(message)
+                .data(data)
+                .fail(false)
+                .success(true)
+                .build();
+    }
+
+    public static CommonResponse fail(ErrorCode errorCode) {
+        return CommonResponse.builder()
+                .resultCode("F-1")
+                .msg(errorCode.getErrorMsg())
+                .fail(false)
+                .success(true)
+                .build();
+    }
+}
