3fe6c75bb12e48c37cf3a36955c767499a11c310
jhdl0157 <jhdl0157@naver.com>
2022-11-07 10:46:28 +0900

    Feat : 유저디테일 서비스 통과시 해줄 성공 핸들러 추가 여기서 토큰이 만들어진다.

---
+++ 4Week_Mission/src/main/java/com/example/finalProject/security/handler/APILoginSuccessHandler.java
@@ -0,0 +1,54 @@
+package com.example.finalProject.security.handler;
+
+import com.example.finalProject.common.response.CommonResponse;
+import com.example.finalProject.common.util.JWTUtil;
+import com.example.finalProject.domain.member.Member;
+import com.google.gson.Gson;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+@Log4j2
+@RequiredArgsConstructor
+public class APILoginSuccessHandler implements AuthenticationSuccessHandler {
+
+    private final JWTUtil jwtUtil;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
+
+        log.info("Login Success Handler......................");
+
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        log.info(authentication);
+        log.info(authentication.getName()); //username
+
+        Map<String, Object> claim = Map.of("mid", authentication.getName());
+        //Access Token 유효기간 1일
+        String accessToken = jwtUtil.generateToken(claim, 1);
+        //Refresh Token 유효기간 30일
+        String refreshToken = jwtUtil.generateToken(claim, 10);
+
+        Gson gson = new Gson();
+
+        Map<String,String> keyMap = Map.of(
+                "accessToken", accessToken,
+                "refreshToken", refreshToken);
+
+        String jsonStr = gson.toJson(CommonResponse.success(keyMap,"로그인 성공, Access Token을 발급합니다.로그인 성공, Access Token을 발급합니다.") );
+
+        response.getWriter().println(jsonStr);
+
+    }
+}
