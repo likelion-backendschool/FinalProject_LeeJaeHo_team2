9312616049dc6426fd53d256e4ebbb8b7461934f
jhdl0157 <jhdl0157@naver.com>
2022-11-08 10:08:13 +0900

    Feat : 사용자 등록 API 추가 틀만 잡고 추후 상세 구현

--- 4Week_Mission/src/main/java/com/example/finalProject/interfaces/member/MemberApiController.java
+++ 4Week_Mission/src/main/java/com/example/finalProject/interfaces/member/MemberApiController.java
@@ -1,16 +1,21 @@
 package com.example.finalProject.interfaces.member;
 
+import com.example.finalProject.application.MemberFacade;
 import com.example.finalProject.common.response.CommonResponse;
 import com.example.finalProject.domain.member.Member;
 import com.example.finalProject.infrastructure.member.MemberRepository;
 import com.example.finalProject.security.APIUserDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.Map;
 
 @Slf4j
@@ -19,10 +24,20 @@ import java.util.Map;
 @RequestMapping("/api/v1/member")
 public class MemberApiController {
     private  final MemberRepository memberRepository;
+    private  final MemberFacade memberFacade;
     @GetMapping("/me")
-    public CommonResponse getMemberInfo(@AuthenticationPrincipal APIUserDTO apiUserDTO){
+    public ResponseEntity<CommonResponse> getMemberInfo(@AuthenticationPrincipal APIUserDTO apiUserDTO){
         System.out.println("유저 정보 나갑니다!!!");
         var user=memberRepository.findByUsername(apiUserDTO.getUsername()).orElseThrow();
-        return CommonResponse.success(Map.of("member",new MemberDto.MemberResponse(user)),"성공");
+        return new ResponseEntity<>(CommonResponse.success(Map.of("member",new MemberDto.MemberResponse(user)),"성공"), HttpStatus.OK);
     }
+
+    @PostMapping("/register")
+    public ResponseEntity<CommonResponse> registerMember(@Valid MemberDto.RegisterRequest registerRequest){
+        var command=registerRequest.toCommand();
+        var memberInfo=memberFacade.registerMember(command);
+        var response=new MemberDto.MemberResponse(memberInfo);
+        return new ResponseEntity<>(CommonResponse.success(response,"성공"),HttpStatus.OK);
+    }
+
 }
