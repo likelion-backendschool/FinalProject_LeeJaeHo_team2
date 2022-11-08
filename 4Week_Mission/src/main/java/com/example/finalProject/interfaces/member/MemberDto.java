ca9e452687e786fa082ff1abb483552dfdcecc42
jhdl0157 <jhdl0157@naver.com>
2022-11-08 10:10:11 +0900

    Feat : 사용자 등록시 Request 클래스 도입 valid적용

--- 4Week_Mission/src/main/java/com/example/finalProject/interfaces/member/MemberDto.java
+++ 4Week_Mission/src/main/java/com/example/finalProject/interfaces/member/MemberDto.java
@@ -2,9 +2,15 @@ package com.example.finalProject.interfaces.member;
 
 import com.example.finalProject.common.util.Util;
 import com.example.finalProject.domain.member.Member;
+import com.example.finalProject.domain.member.MemberCommand;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -12,6 +18,31 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class MemberDto {
+
+    @Getter
+    @Setter
+    @ToString
+    @RequiredArgsConstructor
+    public static class RegisterRequest {
+        private final PasswordEncoder passwordEncoder;
+        @NotEmpty(message = "partnerName 는 필수값입니다")
+        private String memberName;
+
+        @NotEmpty(message = "password 는 필수값입니다")
+        private String password;
+
+        @Email(message = "email 형식에 맞아야 합니다")
+        @NotEmpty(message = "email 는 필수값입니다")
+        private String email;
+
+        public MemberCommand toCommand() {
+            return MemberCommand.builder()
+                    .userName(memberName)
+                    .password(passwordEncoder.encode(password))
+                    .emil(email)
+                    .build();
+        }
+    }
     @Getter
     @ToString
     public static class MemberResponse{
