a6ffe955f4304bebe11323b58044a76323212a2e
jhdl0157 <jhdl0157@naver.com>
2022-11-08 20:52:53 +0900

    Feat : 멤버 저장을 담당하는 클래스 작성

--- 4Week_Mission/src/main/java/com/example/finalProject/domain/member/MemberServiceImpl.java
+++ 4Week_Mission/src/main/java/com/example/finalProject/domain/member/MemberServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.finalProject.domain.member;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -8,10 +9,11 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class MemberServiceImpl implements  MemberService{
     private final MemberStore memberStore;
+    private final PasswordEncoder passwordEncoder;
     @Override
     @Transactional
     public Member registerPartner(MemberCommand command) {
-        var initMember=command.toEntity();
+        var initMember=command.toEntity(passwordEncoder);
         Member member=memberStore.store(initMember);
         return member;
     }
